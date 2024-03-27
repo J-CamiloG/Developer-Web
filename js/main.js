a56bf7ec2136b24e402a5dc3d581ab6f5eb2e4fd
J-CamiloG <jc6381878@gmail.com>
2024-03-27 15:22:01 -0500

    section expirience and slider tools

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
- const typed = new Typed('.typed',{
-     strings: ["Full Stack", "Front End", "Back End"],
+//   const typed = new Typed('.typed',{
+//       strings: ["Full Stack", "Front End", "Back End"],
 
-     typeSpeed: 75,
-     startDelay: 300,
-     backSpeed: 75,
-     loop:true,
-     cursorChar: '|',
+//       typeSpeed: 75,
+//      startDelay: 300,
+//       backSpeed: 75,
+//      loop:true,
+//      cursorChar: '|',
 
-})
+// })
+
