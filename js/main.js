dc56505a685b11c1b4034123db6d61b5a250c934
J-CamiloG <jc6381878@gmail.com>
2024-03-26 21:52:56 -0500

    section ptojects

---
+++ js/main.js
@@ -0,0 +1,10 @@
+ const typed = new Typed('.typed',{
+     strings: ["Full Stack", "Front End", "Back End"],
+
+     typeSpeed: 75,
+     startDelay: 300,
+     backSpeed: 75,
+     loop:true,
+     cursorChar: '|',
+
+})
