389945c2c6aa10a15a4d0562e3cec99812b9e13c
J-CamiloG <jc6381878@gmail.com>
2024-03-28 14:45:24 -0500

    Correcion de carpeta public y correcion de from

--- js/main.js
+++ js/main.js
@@ -1,11 +1,47 @@
-//   const typed = new Typed('.typed',{
-//       strings: ["Full Stack", "Front End", "Back End"],
+const typed = new Typed('.typed',{
+    strings: ["Full Stack", "Front End", "Back End"],
 
-//       typeSpeed: 75,
-//      startDelay: 300,
-//       backSpeed: 75,
-//      loop:true,
-//      cursorChar: '|',
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    loop:true,
+    cursorChar: '|',
+})
+const form = document.getElementById( 'form' );
 
-// })
 
+function sendMail(e){
+    e.preventDefault();
+    
+    let name = document.getElementById('name').value
+    let email = document.getElementById('email').value
+    let message = document.getElementById('comentario').value
+
+    if (!name || !email || !message) {
+        alert("Por favor llenar los campos")
+        return
+    }
+
+    let params = {
+        name, 
+        email, 
+        message
+    }
+
+    const serviceID = 'service_8n905uh';
+    const template = 'template_yk2bsco';
+
+    emailjs.send(serviceID,template,params)
+    .then(
+        res =>{
+            document.getElementById('name').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('comentario').value ='';
+            console.log(res);
+            alert('realizado')
+        }
+    )
+    .catch((err) => console.log(err));
+}
+
+form.addEventListener('submit',sendMail)
--- mail.php
+++
@@ -1,7 +0,0 @@
-<?php
-isset($_POST['nombre'])
-$nombre = $_POST[‘name’];
-$asunto = $_POST[‘subject’];
-$mensaje = $_POST['comments'];
-var_dump($nombre)
-mail('jc6381878@gmail.com', "Mensaje web: $asunto", $mensaje)
